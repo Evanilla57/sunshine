fc0315a04375525b41a1207c6e2d9599301d42ee
Evanilla57 <grigg005@cougars.csusm.edu>
2023-08-06 23:03:46 -0700

    added moment js script to html, added code to log forecast based on current position

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,5 @@
-var apiKey = 'f5a003f8e333c73009944afc5cfcf5b8';
+// Variables
+var apiKey = '948c68d5e82bcef7d9b883a9094fe684';
 var searchEl = document.getElementById('search');
 var cityEl = document.getElementById('city');
 var searchBtn = document.getElementById('search-btn');
@@ -10,44 +11,10 @@ var searchInput;
 // http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
 // http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 
-function load() {
-    var loadHistory = localStorage.getItem('searchLocal')
-    if (loadHistory == null) {
-        return;
-    };
-    var cityArray = JSON.parse(loadHistory)
-    for (var i = 0; i < cityArray.length; i++) {
-        var localBtn = document.createElement('button');
-        localBtn.classList.add('local-btn')
-        localBtn.textContent = cityArray[i]
-        historyBtn.append(localBtn);
-    }
-};
-
-var weekday = [
-    moment().format('dddd'),
-    moment().add(1, 'd').format('dddd'),
-    moment().add(2, 'd').format('dddd'),
-    moment().add(3, 'd').format('dddd'),
-    moment().add(4, 'd').format('dddd'),
-    moment().add(5, 'd').format('dddd'),
-    moment().add(6, 'd').format('dddd'),
-]
-
-for (i = 0; i < 6; i++) {
-    document.getElementById('day-' + i + '').textContent = weekday[i];
-}
-
-function storeHistory(event) {
-    if (!searchEl.value) {
-        return;
-    };
-    searchLocal.push(event);
-    localStorage.setItem('searchLocal', JSON.stringify(searchLocal));
-};
-
+// Calls load function
 load();
 
+// Displays weather forecast based on search input
 function targetCity() {
     if (!searchInput) {
         geoCode = 'https://api.openweathermap.org/geo/1.0/direct?q=' + searchEl.value + '&limit=1&appid=' + apiKey + '';
@@ -58,7 +25,7 @@ function targetCity() {
         storeHistory(searchEl.value);
     } else {
         geoCode = 'https://api.openweathermap.org/geo/1.0/direct?q=' + cityEl + '&limit=1&appid=' + apiKey + '';
-        storeHistory(cityEl)
+        storeHistory(cityEl);
     }
     cityEl = '';
     fetch(geoCode)
@@ -90,9 +57,83 @@ function targetCity() {
         });
 };
 
+// Event Listeners
 searchBtn.addEventListener('click', targetCity);
 
 searchBtn.addEventListener('click', (event) => {
     searchInput = event.target.textContent;
     targetCity();
 }) 
+
+// Displays weekday
+var weekday = [
+    moment().format('dddd'),
+    moment().add(1, 'd').format('dddd'),
+    moment().add(2, 'd').format('dddd'),
+    moment().add(3, 'd').format('dddd'),
+    moment().add(4, 'd').format('dddd'),
+    moment().add(5, 'd').format('dddd'),
+    moment().add(6, 'd').format('dddd'),
+]
+
+for (i = 0; i < 6; i++) {
+    document.getElementById('day-' + i + '').textContent = weekday[i];
+}
+
+// Stores search data to local history
+function storeHistory(event) {
+    if (!searchEl.value) {
+        return;
+    };
+    searchLocal.push(event);
+    localStorage.setItem('searchLocal', JSON.stringify(searchLocal));
+};
+
+function load() {
+    var loadHistory = localStorage.getItem('searchLocal')
+    if (loadHistory == null) {
+        return;
+    };
+    var cityArray = JSON.parse(loadHistory)
+    for (var i = 0; i < cityArray.length; i++) {
+        var localBtn = document.createElement('button');
+        localBtn.classList.add('local-btn')
+        localBtn.textContent = cityArray[i]
+        historyBtn.append(localBtn);
+    }
+};
+
+// Logs forecast based on current position
+window.addEventListener('load', () => {
+    var lat;
+    var lon;
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            lon = position.coords.longitude;
+            lat = position.coords.latitude;
+            queryURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=' + apiKey + '&units=imperial';
+            fetch(queryURL)
+                .then(function (response) {
+                    return response.json()
+                })
+                .then(function (data) {
+                    cityEl.textContent = data.city.name;
+                    for (var i = 0; i < 6; i++) {
+                        document.getElementById('temp-' + i + '').textContent = 'Temp: ' + Number(data.list[i].main.temp).toFixed(0) + '°';
+                    }
+                    for (var i = 0; i < 6; i++) {
+                        document.getElementById('wind-' + i + '').textContent = 'Wind: ' + Number(data.list[i].wind.speed).toFixed(0) + ' mph';
+                    }
+                    for (var i = 0; i < 6; i++) {
+                        document.getElementById('hum-' + i + '').textContent = 'Humidity: ' + Number(data.list[i].main.humidity) + '%';
+                    }
+                    for (var i = 0; i < 6; i++) {
+                        document.getElementById('icon-' + i + '').src = 'https://openweathermap.org/img/wn/' + data.list[i].weather[0].icon + '.png';
+                    }
+                })
+        });
+    }
+})
+
+console.log(lon);
