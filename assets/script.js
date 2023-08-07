0de51f7eda97ea86a8c535dd06790cea00d0c688
Evanilla57 <grigg005@cougars.csusm.edu>
2023-08-06 21:13:25 -0700

    added variables to target ids from html

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,8 @@
-var apiKey = 'f5a003f8e333c73009944afc5cfcf5b8'
+var apiKey = 'f5a003f8e333c73009944afc5cfcf5b8';
+var searchEl = document.getElementById('search');
+var cityEl = document.getElementById('city');
+var searchBtn = document.getElementById('search-btn');
+
+
+// http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+// http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
