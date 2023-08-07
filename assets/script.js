f1bb8057612e2e2d7848fc126f03c267ee141676
Evanilla57 <grigg005@cougars.csusm.edu>
2023-08-06 22:16:00 -0700

    added function ability to dynamically display weather information and icon

--- assets/script.js
+++ assets/script.js
@@ -12,23 +12,23 @@ var searchInput;
 function storeHistory(event) {
     if (!searchBox.value) {
         return;
-    };    
+    };
     searchHistory.push(event);
     localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
 };
 
 function targetCity() {
-    if(!searchInput) {
+    if (!searchInput) {
         geoCode = 'https://api.openweathermap.org/geo/1.0/direct?q=' + searchEl.value + '&limit=1&appid=' + APIKey + '';
         var historyBtn = document.createElement('button');
         historyBtn.classList.add('history-btn')
         historyBtn.textContent = searchEl.value
         btnHolder.append(historyBtn);
         storeHistory(searchEl.value)
-    }else {
-            geoCode = 'https://api.openweathermap.org/geo/1.0/direct?q=' + cityName + '&limit=1&appid=' + APIKey + '';
-            storeHistory(cityName)
-        }
+    } else {
+        geoCode = 'https://api.openweathermap.org/geo/1.0/direct?q=' + cityName + '&limit=1&appid=' + APIKey + '';
+        storeHistory(cityName)
+    }
     cityName = ''
     fetch(geoCode)
         .then(function (response) {
@@ -43,14 +43,25 @@ function targetCity() {
                     return response.json();
                 })
                 .then(function (data) {
-                    
+                    for (var i = 0; i < 6; i++) {
+                        document.getElementById('temp-' + i + '').textContent = 'Temp: ' + Number(data.list[i].main.temp).toFixed(0) + '°';
+                    }
+                    for (var i = 0; i < 6; i++) {
+                        document.getElementById('wind-' + i + '').textContent = 'Wind: ' + Number(data.list[i].wind.speed).toFixed(0) + ' mph';
+                    }
+                    for (var i = 0; i < 6; i++) {
+                        document.getElementById('humid-' + i + '').textContent = 'Humidity: ' + Number(data.list[i].main.humidity) + '%';
+                    }
+                    for (var i = 0; i < 6; i++) {
+                        document.getElementById('icon-' + i + '').src = 'https://openweathermap.org/img/wn/' + data.list[i].weather[0].icon + '.png';
+                    }
                 })
         });
 };
 
 searchBtn.addEventListener('click', targetCity);
 
-searchBtn.addEventListener('click', (event)=> {
+searchBtn.addEventListener('click', (event) => {
     searchInput = event.target.textContent;
     targetCity();
 }) 
